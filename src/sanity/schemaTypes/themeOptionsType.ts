c2534207ce5736b1aef6522cf8b3c14d58826b51
Michael Dinerstein <md@missionmike.dev>
2024-10-24 13:03:56 -0700

    feat: og theme options

--- src/sanity/schemaTypes/themeOptionsType.ts
+++ src/sanity/schemaTypes/themeOptionsType.ts
@@ -11,5 +11,20 @@ export const themeOptionsType = {
       name: "userTitle",
       type: "string",
     },
+    {
+      name: "siteTitle",
+      type: "string",
+      description: "The title is used for HTML page title as well as opengraph.",
+    },
+    {
+      name: "siteDescription",
+      type: "text",
+      description: "This description will appear in the site meta and opengraph.",
+    },
+    {
+      name: "siteImage",
+      type: "image",
+      description: "This image is used in opengraph site previews.",
+    },
   ],
 };
