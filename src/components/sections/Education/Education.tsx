43aeca022801163611735f212bb28b892bfa611a
Michael Dinerstein <md@missionmike.dev>
2024-10-23 23:57:34 -0700

    feat: group by school name

--- src/components/sections/Education/Education.tsx
+++ src/components/sections/Education/Education.tsx
@@ -1,24 +1,42 @@
 "use client";
 
+import React, { useContext } from "react";
+
 import { DataContext } from "@/context/DataContext";
+import type { Education as EducationType } from "../../../../sanity.types";
 import { formatDate } from "@/lib/format";
 import styles from "./Education.module.scss";
-import { useContext } from "react";
 
 export const Education = () => {
   const { education } = useContext(DataContext);
 
+  // Group education by school name. This was a simpler
+  // approach than to create a separate type speficially to group
+  // by school in Sanity.
+  const educationGroupedBySchool: { [key: string]: EducationType[] } = {};
+  education.map((edu) => {
+    if (!edu?.school) return;
+
+    educationGroupedBySchool[edu.school] = educationGroupedBySchool[edu.school]
+      ? [...educationGroupedBySchool[edu.school], edu]
+      : [edu];
+  });
+
   return (
     <section className={styles.section}>
       <h2>Education</h2>
-      {education.map((edu) => (
-        <div key={`education-${edu._id}`}>
-          <h3>{edu.school}</h3>
-          <h4>
-            {edu.award}
-            <span> &mdash; {formatDate(edu?.dateAwarded)}</span>
-          </h4>
-        </div>
+      {Object.keys(educationGroupedBySchool).map((school) => (
+        <React.Fragment>
+          <h3>{school}</h3>
+          {educationGroupedBySchool[school].map((edu) => (
+            <div key={`education-${edu._id}`}>
+              <h4>
+                {edu.award}
+                <span> &mdash; {formatDate(edu?.dateAwarded)}</span>
+              </h4>
+            </div>
+          ))}
+        </React.Fragment>
       ))}
     </section>
   );
