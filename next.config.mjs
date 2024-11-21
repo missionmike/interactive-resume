b612cc16603648da7dac5398f33e37cce95631d2
Michael Dinerstein <md@missionmike.dev>
2024-11-21 15:25:40 -0800

    fix: test updated config.

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,22 @@ const siteHostingProvider = process?.env?.SITE_HOST;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // For GitHub hosting, the build should be output to the "out" folder.
   output: siteHostingProvider === "github" ? "export" : undefined,
+
+  async exportPathMap(defaultPathMap) {
+    // Remove the Studio page when hosted on GitHub.
+    if (siteHostingProvider === "github") {
+      const { "/studio/[[...tool]]": _, ...filteredPaths } = defaultPathMap;
+      return filteredPaths;
+    }
+
+    // Include all paths if not on GitHub
+    return defaultPathMap;
+  },
+
   images: {
+    // Images need to be unoptimized on GitHub pages.
     unoptimized: siteHostingProvider === "github",
     remotePatterns: [
       {
