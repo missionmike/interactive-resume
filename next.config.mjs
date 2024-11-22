26059e8df59e16518f2f1e103460eb31a78c8413
Michael Dinerstein <md@missionmike.dev>
2024-11-21 16:11:13 -0800

    fix: remove GitHub pages attempt.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-const siteHostingProvider = process?.env?.SITE_HOST;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For GitHub hosting, the build should be output to the "out" folder.
-  output: siteHostingProvider === "github" ? "export" : undefined,
-
   images: {
-    // Images need to be unoptimized on GitHub pages.
-    unoptimized: siteHostingProvider === "github",
     remotePatterns: [
       {
         protocol: "https",
